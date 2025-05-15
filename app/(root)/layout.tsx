3e42a4a6cf97f6fefd5e79c75c0df91a64f85ec6
Bryan Dizon <bryanpascuadizon@gmail.com>
2025-05-15 13:55:17 +0800

    added suspense tag for daily transactions

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 import { APP_DESCRIPTION, APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: `${APP_NAME}`,
@@ -17,7 +18,7 @@ export default function RootLayout({
         <Header />
         <main className="flex-1 wrapper flex flex-col overflow-hidden">
           <div className="content-wrapper flex flex-col flex-1 overflow-hidden">
-            {children}
+            <Suspense>{children}</Suspense>
           </div>
         </main>
         <Footer />
