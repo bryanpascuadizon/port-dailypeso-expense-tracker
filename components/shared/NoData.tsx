ee263cb2cc7753b11705b668179e82f89a02612c
Bryan Dizon <bryanpascuadizon@gmail.com>
2025-05-17 00:16:51 +0800

    minor ui improvements

--- components/shared/NoData.tsx
+++ components/shared/NoData.tsx
@@ -3,8 +3,8 @@ import { HandCoins } from "lucide-react";
 const NoData = () => {
   return (
     <div className="w-full py-10 text-center text-gray-700">
-      <HandCoins className="w-20 h-20  m-auto  mb-5" />
-      <p className="text-sm">No data available</p>
+      <HandCoins className="w-18 h-18 md:w-20 md:h-20  m-auto  mb-5" />
+      <p className="text-xs md:text-sm">No data available</p>
     </div>
   );
 };
