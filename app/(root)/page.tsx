184dc928f64938952beb37e1c785d46a762452ea
Bryan Dizon <bryanpascuadizon@gmail.com>
2025-05-20 12:27:49 +0800

    remove redirects in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,5 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
-
 const Home = async () => {
-  const session = await auth();
-
-  if (!session) {
-    return redirect("/sign-in");
-  } else {
-    return redirect("/transactions/daily");
-  }
+  return null;
 };
 
 export default Home;
