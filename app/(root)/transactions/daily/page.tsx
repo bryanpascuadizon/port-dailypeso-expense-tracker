3b233da28466f149ec57de9d9bc2286ff29c5a2a
Bryan Dizon <bryanpascuadizon@gmail.com>
2025-05-20 12:48:37 +0800

    remove redirects in daily transaction home page

--- app/(root)/transactions/daily/page.tsx
+++ app/(root)/transactions/daily/page.tsx
@@ -1,14 +1,6 @@
-import { auth } from "@/auth";
 import DailyTransactions from "@/components/shared/Transactions/daily/DailyTransactions";
-import { redirect } from "next/navigation";
 
-const DailyTransactionsPage = async () => {
-  const session = await auth();
-
-  if (!session) {
-    return redirect("/sign-in");
-  }
-
+const DailyTransactionsPage = () => {
   return <DailyTransactions />;
 };
 
