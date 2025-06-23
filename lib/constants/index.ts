cdab521eec142ab4db9e04d9212c4a7a141a9458
Bryan Dizon <bryanpascuadizon@gmail.com>
2025-06-23 23:19:10 +0800

    revise app description

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,7 +1,8 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "DailyPeso";
 
 export const APP_DESCRIPTION =
-  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Simplify Spending Daily";
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "DailyPeso is a personal expense tracker built for effortless daily money management. Users can quickly log expenses and income, categorize transactions by account, and gain clear insights into their spending habits. Designed with a clean UI and a smooth, responsive experience, DailyPeso helps users stay organized, stick to budgets, and make smarter financial decisions — whether they're saving, budgeting, or tracking everyday costs.";
 
 export const MONTHS = [
   "January",
